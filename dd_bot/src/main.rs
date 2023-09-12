17a6660848209a41ba671d103ba80c218ef4e04f
Charlee <charleegithub@gmail.com>
2023-09-12 14:41:40 +0200

    cleaned up code

--- dd_bot/src/main.rs
+++ dd_bot/src/main.rs
@@ -38,33 +38,6 @@ fn main() {
     let steps = rng.gen_range(50..100);
 
     let screen_size = enigo.main_display_size();
-
-    // If 0 then the control point will be (960, 0) (upper) else (960, full height of screen)
-    let upper_or_under = rng.gen_range(0..1);
-
-    let start_x = 100;
-    let start_y = 100;
-    let control_x = screen_size.0 / 2; // Control point for the Bézier curve
-    let control_y: i32; // Control point for the Bézier curve
-
-    // upper
-    if upper_or_under == 0 {
-        control_y = 0;
-    }
-    // lower
-    else {
-        control_y = screen_size.1;
-    }
-
-    let end_x = 1000;
-    let end_y = 100;
-    
-    println!(
-        "Start: {:?}\nEnd: {:?}\nControl: {:?}",
-        (start_x, start_y),
-        (end_x, end_y),
-        (control_x, control_y)
-    );
     
     start_game(&mut enigo, "blacksmith");
 
@@ -107,9 +80,9 @@ fn main() {
 
 fn start_game(enigo: &mut Enigo, launcher_name: &str) {
     enigo.key_click(Key::Meta);
-    sleep(Duration::from_millis(5000));
+    sleep(Duration::from_millis(2000));
     enigo.key_sequence_parse(launcher_name);
-    sleep(Duration::from_millis(5000));
+    sleep(Duration::from_millis(2000));
     enigo.key_click(Key::Return);
     sleep(Duration::from_millis(60000));
 }
