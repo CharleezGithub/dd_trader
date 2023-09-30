583197b9ba84507e19d268a8a47475b9aa881ffb
Charlee <charleegithub@gmail.com>
2023-09-30 14:10:40 +0200

    traceback in error func and more errors caught

--- discord_bot/helpers/stitching.py
+++ discord_bot/helpers/stitching.py
@@ -3,6 +3,7 @@ from PIL import ImageDraw, ImageFont
 import requests
 from io import BytesIO
 
+
 async def stitch_images(user1_urls, user2_urls):
     """Stitch together images from the provided URLs for both users."""
     user1_images = [
@@ -13,22 +14,30 @@ async def stitch_images(user1_urls, user2_urls):
     ]
 
     # Padding values (change these to adjust the space)
-    arrow_padding = 50  # Added space on each side of the arrow
+    arrow_padding_left = 70  # Added space on the left side of the arrow
+    arrow_padding_right = 100  # Added space on the right side of the arrow
     side_padding = 20  # Added space on each side of the items
 
     # Determine max width and total height for each user's images
     max_width_user1 = max(img.width for img in user1_images) + 2 * side_padding
     max_width_user2 = max(img.width for img in user2_images) + 2 * side_padding
+
     total_height_user1 = sum(img.height for img in user1_images)
     total_height_user2 = sum(img.height for img in user2_images)
 
+    # If both user1_images and user2_images are empty, return None or some default image
+    if not max_width_user1 and not max_width_user2:
+        return None
+
     # Create an arrow image with added padding and the same height as the tallest column
     max_height = max(total_height_user1, total_height_user2)
-    arrow_image_width = 50 + 2 * arrow_padding
+    arrow_image_width = 50 + arrow_padding_left + arrow_padding_right
     arrow_image = Image.new("RGB", (arrow_image_width, max_height), color="white")
     draw = ImageDraw.Draw(arrow_image)
     font = ImageFont.truetype("arial.ttf", 50)
-    draw.text((arrow_padding, (max_height - 35) // 2), "<--->", font=font, fill="black")
+    draw.text(
+        (arrow_padding_left, (max_height - 35) // 2), "<===>", font=font, fill="black"
+    )
 
     # Create the final stitched image
     total_width = max_width_user1 + arrow_image.width + max_width_user2
