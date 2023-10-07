99d021a9299d47b49d38052201c7822f38ec5e3d
Charlee <charleegithub@gmail.com>
2023-10-07 12:24:53 +0200

    reverted back to old detection test

--- dd_bot/python_helpers/obj_detect_test.py
+++ dd_bot/python_helpers/obj_detect_test.py
@@ -12,7 +12,7 @@ if len(sys.argv) > 1:
     image_name = sys.argv[1]
 else:
     image_name = "images/play.png"
-    image_name = "python_helpers/images/gold_fee4.png"
+    image_name = "python_helpers/images/gold_fee_double_check.png"
 
 max_val = 0.00
 
@@ -20,39 +20,67 @@ max_val = 0.00
 
 tries = 0
 
-# Lowering the threshold slightly
-match_threshold = 0.85  
 
-# Multi-scale template matching: considering scales from 80% to 120%
-scales = np.linspace(0.8, 1.2, 5)  
-
-while max_val < match_threshold:
+while max_val < 0.90:
     print("hello")
+    # If it has tried for 4 minutes then break
     if tries > 240:
         break
-    
+    # Capture a screenshot using ImageGrab
     screenshot = ImageGrab.grab()
+
+    # Convert the screenshot to an OpenCV format
     main_image = np.array(screenshot)
+
+    # Convert the screenshot image from BGR to RGB (OpenCV loads images in BGR by default)
     main_image = cv2.cvtColor(main_image, cv2.COLOR_BGR2RGB)
-    
+
+    # Load the template
     template = cv2.imread(image_name, cv2.IMREAD_COLOR)
-    
-    # Apply Gaussian blur to the template and main image
-    main_image = cv2.GaussianBlur(main_image, (5, 5), 0)
-    template = cv2.GaussianBlur(template, (5, 5), 0)
-    
-    for scale in scales:
-        # Resize the template according to the scale
-        resized_template = cv2.resize(template, (int(template.shape[1] * scale), int(template.shape[0] * scale)))
-        
-        result = cv2.matchTemplate(main_image, resized_template, cv2.TM_CCOEFF_NORMED)
-        _, max_val, _, max_loc = cv2.minMaxLoc(result)
-        
-        if max_val > match_threshold:
-            break  # If a match is found, exit the loop early
-    
+
+    # Use template matching
+    result = cv2.matchTemplate(main_image, template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
     tries += 1
     time.sleep(1)
+    print(f"Certainty Score: {max_val:.2f}")
+
+
+if tries < 240:
+    # Get the top-left corner of the matched area
+    top_left = max_loc
+    bottom_right = (top_left[0] + template.shape[1], top_left[1] + template.shape[0])
+
+    # Draw a rectangle around the matched object
+    cv2.rectangle(
+        main_image, top_left, bottom_right, (0, 255, 0), 2
+    )  # Change rectangle color to green for visibility
+
+    # Display the result in a named window
+    window_name = "Detected Object"
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    # Set the window to fullscreen
+    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    cv2.imshow(window_name, main_image)
+
+    # Get the screen coordinates of the window
+    x, y, w, h = cv2.getWindowImageRect(window_name)
+
+    # Calculate the screen coordinates of the detected object
+    screen_top_left = (x + top_left[0], y + top_left[1])
+    screen_bottom_right = (x + bottom_right[0], y + bottom_right[1])
+
+    # Top left coords, Bottom right coords. x1 y1 x2 y2
+    print(
+        screen_top_left[0],
+        screen_top_left[1],
+        screen_bottom_right[0],
+        screen_bottom_right[1],
+    )
+
+    # Print the certainty score (i.e., the maximum correlation coefficient)
     print(f"Certainty Score: {max_val:.2f}")
 
 
