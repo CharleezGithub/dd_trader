69e98029b396a5df967e38860bd1ce43b020029d
Charlee <charleegithub@gmail.com>
2023-10-08 10:26:18 +0200

    escrow check if both players items are in escor

--- discord_bot/helpers/escrow_status.py
+++ discord_bot/helpers/escrow_status.py
@@ -1,20 +1,24 @@
 import sqlite3
 
+
 def check_all_items_in_escrow(channel_id):
     # Replace with your database path
-    conn = sqlite3.connect("trading_bot_test.db")
+    conn = sqlite3.connect("trading_bot.db")
     cursor = conn.cursor()
-    
+
     try:
-        cursor.execute("""
+        cursor.execute(
+            """
             SELECT status
             FROM items
             JOIN trades ON items.trade_id = trades.id
             WHERE trades.channel_id = ?
-        """, (channel_id,))
-        
+        """,
+            (channel_id,),
+        )
+
         statuses = cursor.fetchall()
-        
+
         # Check if all item statuses in the trade are "in escrow"
         return all(status[0] == "in escrow" for status in statuses)
     except sqlite3.Error as e:
