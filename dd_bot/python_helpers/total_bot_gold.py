76bcf46d7f604fdafa19c31214586b244d8ea84e
Charlee <charleegithub@gmail.com>
2023-10-23 08:43:32 +0200

    added a total gold for the bots gold

---
+++ dd_bot/python_helpers/total_bot_gold.py
@@ -0,0 +1,28 @@
+import pytesseract
+from PIL import Image, ImageFilter, ImageGrab
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+# Capture a screenshot using ImageGrab
+image = ImageGrab.grab()
+
+crop_box = (1120, 279, 1230, 301)
+
+# Crop the image
+image = image.crop(crop_box)
+
+# Convert the image to grayscale
+image = image.convert("L")
+
+# Display the cropped and grayscale image
+# image.show()
+
+# Use Tesseract to do OCR on the image
+custom_config = r"--oem 3 --psm 6 outputbase digits"  # OEM 3 is both standard and LSTM OCR, and PSM 6 assumes a single uniform block of text.
+text = pytesseract.image_to_string(image, config=custom_config)
+
+# Print the extracted text
+if text.strip() == "":
+    print("No text detected")
+else:
+    print(text.strip())
