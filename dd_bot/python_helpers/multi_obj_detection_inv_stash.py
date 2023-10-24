fba98838ff4f0a9293408bf904931568581b4afa
Charlee <charleegithub@gmail.com>
2023-10-24 14:26:13 +0200

    removed print from detect script

--- dd_bot/python_helpers/multi_obj_detection_inv_stash.py
+++ dd_bot/python_helpers/multi_obj_detection_inv_stash.py
@@ -78,8 +78,6 @@ try:
         result = cv2.matchTemplate(main_image, template, cv2.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
-        print(max_val)
-
         #pil = Image.fromarray(main_image)
         #pil.show()
         tries += 1
