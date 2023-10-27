f901eec2fb7f8e779792c02aa73692a416077a15
Charlee <charleegithub@gmail.com>
2023-10-27 11:42:41 +0200

    fixed paid fee

--- discord_bot/helpers/has_paid_gold_fee.py
+++ discord_bot/helpers/has_paid_gold_fee.py
@@ -1,38 +1,47 @@
 import sqlite3
 
+
 def has_user_paid_fee(discord_id, channel_id):
     # Replace with your database path
     conn = sqlite3.connect("trading_bot.db")
     cursor = conn.cursor()
-    
+
     try:
         # Get trader ID for the given discord_id
-        cursor.execute("""
+        cursor.execute(
+            """
             SELECT id
             FROM traders
             WHERE discord_id = ?
-        """, (discord_id,))
+        """,
+            (discord_id,),
+        )
         result = cursor.fetchone()
         if not result:
             print("User not found")
             return False
         trader_id = result[0]
-        
+
         # Fetch payment status based on trader ID
-        cursor.execute("""
-            SELECT trader1_paid, trader2_paid 
+        cursor.execute(
+            """
+            SELECT trader1_paid, trader2_paid, trader1_id, trader2_id 
             FROM trades 
             WHERE channel_id = ? AND (trader1_id = ? OR trader2_id = ?)
-        """, (channel_id, trader_id, trader_id))
-        
+        """,
+            (channel_id, trader_id, trader_id),
+        )
+
         result = cursor.fetchone()
-        
-        # Check if result is not None and verify payment status
+
         if result:
-            trader1_paid, trader2_paid = result
-            if (trader_id == trader_id and trader1_paid) or (trader_id == trader_id and trader2_paid):
+            trader1_paid, trader2_paid, trader1_id, trader2_id = result
+
+            if trader_id == trader1_id and trader1_paid:
                 return True
-        
+            elif trader_id == trader2_id and trader2_paid:
+                return True
+
         return False
     except sqlite3.Error as e:
         print(f"An error occurred: {e}")
@@ -42,9 +51,9 @@ def has_user_paid_fee(discord_id, channel_id):
 
 
 # Example usage:
-# discord_id = "user_discord_id_here"
-# channel_id = "channel_id_here"
-# if has_user_paid_fee(discord_id, channel_id):
-#     print(f"User {discord_id} has paid the fee for trade in channel {channel_id}!")
-# else:
-#     print(f"User {discord_id} has NOT paid the fee for trade in channel {channel_id}.")
+discord_id = "717964821965963336"
+channel_id = "1167104703516123228"
+if has_user_paid_fee(discord_id, channel_id):
+    print(f"User {discord_id} has paid the fee for trade in channel {channel_id}!")
+else:
+    print(f"User {discord_id} has NOT paid the fee for trade in channel {channel_id}.")
