061f7d948f5eddf5edc8fccf2d399043279d4424
Charlee <charleegithub@gmail.com>
2023-11-12 13:11:54 +0100

    Update main.py

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1702,7 +1702,7 @@ def cancel_trade_check(discord_id, channel_id) -> bool:
     trader2_count = cursor.fetchall()
 
     conn.close()
-    print(f"trader_item_escrow_countt:\n{trader1_item_escrow_count[0][0]}\n{trader2_item_escrow_count[0][0]}")
+    print(f"trader_item_escrow_countt:\n{trader1_item_escrow_count[0]}\n{trader2_item_escrow_count[0]}\n{trader1_id}")
     print(f"gold in escrow:\n{trader1_gold_traded}\n{trader2_gold_traded}")
 
     print(f"trader_count:\n{trader1_count[0][0]}\n{trader2_count[0][0]}")
