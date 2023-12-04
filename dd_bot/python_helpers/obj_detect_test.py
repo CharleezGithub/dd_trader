981d9c54eac94eeb8d271d4d5621ab6ea66e9771
Charlee <charleegithub@gmail.com>
2023-12-04 18:56:50 +0100

    crop arg that scales with the image dimentions

--- dd_bot/python_helpers/obj_detect_test.py
+++ dd_bot/python_helpers/obj_detect_test.py
@@ -20,6 +20,8 @@ longer = False
 # Will grayscale both the image and the screenshot if True.
 grayscale = True
 
+crop = False
+
 if len(sys.argv) > 1 and not len(sys.argv) > 2:
     image_name = sys.argv[1]
 elif len(sys.argv) > 2:
@@ -37,8 +39,10 @@ elif len(sys.argv) > 2:
             longer = True
         elif arg.strip() == "G":
             grayscale = True
+        elif arg.strip() == "CR":
+            crop = True
 else:
-    image_name = "dd_bot/temp_images/item/image.png"
+    image_name = "dd_bot/temp_images/info/image5.png"
 
 max_val = 0.00
 
@@ -79,6 +83,27 @@ while max_val < limit:
     if grayscale:
         main_image = cv2.cvtColor(main_image, cv2.COLOR_RGB2GRAY)
         template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    
+    if crop:
+        # The shape of an opencv image follows this format: Height (Rows), Width (Columns), Channels
+        height, width = template.shape
+
+        # 0.05 means 5% of the image will be cut off from every side of the image.
+        # This can be recalibrated.
+        # Smaller images should be cropped more drasticly as the in-game borders around the item do not change but only the size of the item
+        # Bigger items will get scaled too far.
+        if height < 70:
+            crop_height = int(height * 0.15)
+        else:
+            crop_height = int(height * 0.05)
+        
+        if width < 70:
+            crop_width = int(width * 0.15)
+        else:
+            crop_width = int(width * 0.05)
+
+        # Format of cropping is: [Start height:End Height, Start width:End width]
+        template = template[crop_height:-crop_height, crop_width:-crop_width]
 
     # Use template matching
     result = cv2.matchTemplate(main_image, template, cv2.TM_CCOEFF_NORMED)
@@ -94,14 +119,17 @@ if tries < max_tries:
     top_left = max_loc
     bottom_right = (top_left[0] + template.shape[1], top_left[1] + template.shape[0])
 
+    color_image = cv2.cvtColor(main_image, cv2.COLOR_GRAY2BGR)
+
+
     # Draw a rectangle around the matched object
-    cv2.rectangle(main_image, top_left, bottom_right, (0, 255, 0), 2)
+    cv2.rectangle(color_image, top_left, bottom_right, (0, 255, 0), 1)
 
     # Display the result in a named window
     window_name = "Detected Object"
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
     cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    cv2.imshow(window_name, main_image)
+    cv2.imshow(window_name, color_image)
 
     # Print the coordinates of the detected object directly
     print(top_left[0], top_left[1], bottom_right[0], bottom_right[1])
